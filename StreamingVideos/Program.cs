22e275ad7435be13f84052b4a94b81456de03294
Visar Buza <visar@gjirafa.com>
2020-11-01 14:24:17 +0100

    Init

---
+++ StreamingVideos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamingVideos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Streaming Videos!");
+        }
+    }
+}
